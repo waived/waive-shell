46354a6ac0cf05d8d87b77b987c66584a6e7df73
waived <165493645+waived@users.noreply.github.com>
2024-06-13 22:23:16 -0400

    Update waiveshell.php

--- waiveshell.php
+++ waiveshell.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="WaiveShell1.0" content="DoS Attacker in PHP">
+    <meta name="WaiveShell2.0" content="DoS Attacker in PHP">
     <meta name="author" content="waived">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WAIVE SHELL 2.0</title>
